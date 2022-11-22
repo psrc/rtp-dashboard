e8917399d8e0c3bdbadd7da64ac131d809a08633
grantmgibson <53488250+grantmgibson@users.noreply.github.com>
2022-11-22 12:08:39 -0800

    create race_condensed and add to "grouping" column
    also fixed minor issues in function

--- pums_processing.R
+++ pums_processing.R
@@ -37,34 +37,47 @@ process_pums_data <- function() {
       dplyr::mutate(vehicle_occ = factor(dplyr::case_when(is.na(.data$vehicle_occ) ~ NA_character_,
                                                           .data$vehicle_occ == 1 ~ "Drove alone",
                                                           TRUE ~ "Carpool"))) %>% 
-      dplyr::mutate(travel_mode = factor(case_when(is.na(.data$travel_mode) ~ NA_character_,
-                                                   .data$travel_mode == "Car, truck, or van" & .data$vehicle_occ == "Drove alone" ~ "SOV",
-                                                   .data$travel_mode == "Car, truck, or van" & .data$vehicle_occ == "Carpool" ~ "HOV",
-                                                   TRUE ~ as.character(.data$travel_mode))),
+      dplyr::mutate(travel_mode = factor(dplyr::case_when(is.na(.data$travel_mode) ~ NA_character_,
+                                                          .data$travel_mode == "Car, truck, or van" & .data$vehicle_occ == "Drove alone" ~ "SOV",
+                                                          .data$travel_mode == "Car, truck, or van" & .data$vehicle_occ == "Carpool" ~ "HOV",
+                                                          TRUE ~ as.character(.data$travel_mode))),
                     travel_time = ifelse(is.na(.data$travel_time), NA_integer_, as.integer(.data$travel_time)),
-                    commute_class = factor(case_when(is.na(.data$travel_time) ~ NA_character_,
-                                                     as.integer(.data$travel_time) < 15 ~ "15 mintues or less",
-                                                     as.integer(.data$travel_time) < 30 ~ "30 mintues or less",
-                                                     as.integer(.data$travel_time) < 60 ~ "60 mintues or less",
-                                                     as.integer(.data$travel_time) < 90 ~ "90 mintues or less",
-                                                     as.integer(.data$travel_time) >= 90 ~ "Supercommuter")))
+                    commute_class = factor(dplyr::case_when(is.na(.data$travel_time) ~ NA_character_,
+                                                            as.integer(.data$travel_time) < 15 ~ "15 mintues or less",
+                                                            as.integer(.data$travel_time) < 30 ~ "30 mintues or less",
+                                                            as.integer(.data$travel_time) < 60 ~ "60 mintues or less",
+                                                            as.integer(.data$travel_time) < 90 ~ "90 mintues or less",
+                                                            as.integer(.data$travel_time) >= 90 ~ "Supercommuter")),
+                    race_condensed = factor(dplyr::case_when(.data$race %in% c("American Indian or Alaskan Native Alone",
+                                                                               "Asian alone",
+                                                                               "Black or African American alone",
+                                                                               "Hispanic or Latino",
+                                                                               "Native Hawaiian and Other Pacific Islander alone",
+                                                                               "Some Other Race alone",
+                                                                               "Two or More Races")
+                                                               ~ "POC",
+                                                             .data$race == "White alone" ~ "White")))
     
     # Summarize travel time means by county (incl. region) and race
-    mean_time_county <- psrccensus::psrc_pums_mean(pums, stat_var = "travel_time", group_vars = "COUNTY") %>% 
-      dplyr::transmute(geography = .data$COUNTY,
-                       metric = "mean_commute_time",
-                       data_year = .data$DATA_YEAR,
-                       estimate = dplyr::ends_with("mean"),
-                       moe = dplyr::ends_with("moe"))
+    mean_time_county <- psrccensus::psrc_pums_mean(pums,
+                                                   stat_var = "travel_time",
+                                                   group_vars = "COUNTY") %>% 
+      dplyr::select(geography = .data$COUNTY,
+                    data_year = .data$DATA_YEAR,
+                    estimate = dplyr::ends_with("mean"),
+                    moe = dplyr::ends_with("moe")) %>% 
+      dplyr::mutate(metric = "mean_commute_time", .before = .data$data_year)
     
-    mean_time_race <- psrccensus::psrc_pums_mean(pums, stat_var = "travel_time", group_vars = "race") %>% 
-      dplyr::filter(.data$race != "Total") %>% 
-      dplyr::transmute(geography = .data$COUNTY,
-                       variable = .data$race,
-                       metric = "mean_commute_time",
-                       data_year = .data$DATA_YEAR,
-                       estimate = dplyr::ends_with("mean"),
-                       moe = dplyr::ends_with("moe"))
+    mean_time_race <- psrccensus::psrc_pums_mean(pums,
+                                                 stat_var = "travel_time",
+                                                 group_vars = "race_condensed") %>% 
+      dplyr::filter(.data$race_condensed != "Total") %>% 
+      dplyr::select(geography = .data$COUNTY,
+                    grouping = .data$race_condensed,
+                    data_year = .data$DATA_YEAR,
+                    estimate = dplyr::ends_with("mean"),
+                    moe = dplyr::ends_with("moe")) %>% 
+      dplyr::mutate(metric = "mean_commute_time", .before = .data$data_year)
     
     means <- dplyr::bind_rows(mean_time_county, mean_time_race) %>% 
       dplyr::mutate(data_year = as.character(.data$data_year))
@@ -112,12 +125,13 @@ process_pums_data <- function() {
                        moe = .data$share_moe)
     
     class_race <- psrccensus::psrc_pums_count(pums,
-                                              group_vars = c("race", "commute_class"),
+                                              group_vars = c("race_condensed", "commute_class"),
                                               incl_na = FALSE) %>% 
-      dplyr::filter(.data$race != "Total")
+      dplyr::filter(.data$race_condensed != "Total")
     
     count_class_race <- class_race %>% 
-      dplyr::transmute(geography = .data$race,
+      dplyr::transmute(geography = .data$COUNTY,
+                       grouping = .data$race_condensed,
                        variable = .data$commute_class,
                        metric = "count_commute_class",
                        data_year = .data$DATA_YEAR,
@@ -125,7 +139,8 @@ process_pums_data <- function() {
                        moe = .data$count_moe)
     
     share_class_race <- class_race %>% 
-      dplyr::transmute(geography = .data$race,
+      dplyr::transmute(geography = .data$COUNTY,
+                       grouping = .data$race_condensed,
                        variable = .data$commute_class,
                        metric = "share_commute_class",
                        data_year = .data$DATA_YEAR,
@@ -174,12 +189,13 @@ process_pums_data <- function() {
                        moe = .data$share_moe)
     
     mode_race <- psrccensus::psrc_pums_count(pums,
-                                             group_vars = c("race", "travel_mode"),
+                                             group_vars = c("race_condensed", "travel_mode"),
                                              incl_na = FALSE) %>% 
-      dplyr::filter(.data$race != "Total")
+      dplyr::filter(.data$race_condensed != "Total")
     
     count_mode_race <- mode_race %>% 
-      dplyr::transmute(geography = .data$race,
+      dplyr::transmute(geography = .data$COUNTY,
+                       grouping = .data$race_condensed,
                        variable = .data$travel_mode,
                        metric = "count_travel_mode",
                        data_year = .data$DATA_YEAR,
@@ -187,7 +203,8 @@ process_pums_data <- function() {
                        moe = .data$count_moe)
     
     share_mode_race <- mode_race %>% 
-      dplyr::transmute(geography = .data$race,
+      dplyr::transmute(geography = .data$COUNTY,
+                       grouping = .data$race_condensed,
                        variable = .data$travel_mode,
                        metric = "share_travel_mode",
                        data_year = .data$DATA_YEAR,
@@ -209,7 +226,7 @@ process_pums_data <- function() {
     
   }
   
-  processed <- processed[, c("geography", "variable", "metric", "data_year", "estimate", "moe")]
+  processed <- processed[, c("geography", "grouping", "variable", "metric", "data_year", "estimate", "moe")]
   
   return(processed)
   
