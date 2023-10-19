090fbd570e768b4aaf440a949b08b114175ee93c
Craig Helmann <chelmann@psrc.org>
2023-10-19 10:37:48 -0700

    added deployment app information

---
+++ deploy.R
@@ -0,0 +1,13 @@
+library(rsconnect)
+
+# test version ----
+
+deployApp(account = 'psrcwa',
+          appName = 'development-rtp-dashboard',
+          appTitle = 'Development RTP Dashboard')
+
+# official version ----
+
+deployApp(account = 'psrcwa',
+          appName = 'rtp_dashboard',
+          appTitle = 'RTP Dashboard')
