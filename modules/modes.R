28d6161f31f058a52dab3909c9094afe5e776a9f
Craig Helmann <chelmann@psrc.org>
2023-11-07 10:57:10 -0800

    bike to work charts and text updated

--- modules/modes.R
+++ modules/modes.R
@@ -122,26 +122,71 @@ bike_server <- function(id) {
   moduleServer(id, function(input, output, session){
     ns <- session$ns
     
-    # Text and charts
-    output$commute_mode_bike_est_chart <- renderPlotly({interactive_column_chart(t=data %>% filter(metric=="Simplified Commute Mode" & geography_type=="County" & variable=="Bicycle"),
-                                                                                 y='estimate', x='geography', moe="estimate_moe", fill='data_year', pos = "dodge",
-                                                                                 est="number", color='pgnobgy_10')})
+    # Text
+    output$bike_share_county_text <- renderText({page_information(tbl=page_text, page_name="Modes", page_section = "Bike-County", page_info = "description")})
+    output$bike_share_race_text <- renderText({page_information(tbl=page_text, page_name="Modes", page_section = "Bike-Race", page_info = "description")})
+    output$bike_share_metro_text <- renderText({page_information(tbl=page_text, page_name="Modes", page_section = "Bike-Metro", page_info = "description")})
+    output$bike_share_city_text <- renderText({page_information(tbl=page_text, page_name="Modes", page_section = "Bike-City", page_info = "description")})
     
-    output$commute_mode_bike_share_chart <- renderPlotly({interactive_column_chart(t=data %>% filter(metric=="Simplified Commute Mode" & geography_type=="County" & variable=="Bicycle"),
-                                                                                   y='share', x='geography', moe="share_moe", fill='data_year', pos = "dodge",
-                                                                                   est="percent", dec=1, color='pgnobgy_10')})
+    # Charts
+    output$bike_ms_county_chart <- renderEcharts4r({echart_column_chart_timeline(df = commute_data |> 
+                                                                                   filter(variable == "Bike" & geography_type == "County" & metric == "commute-modes") |>
+                                                                                   mutate(geography = str_wrap(geography, 10)) |>
+                                                                                   mutate(year = factor(x=year, levels = yr_ord)) |>
+                                                                                   mutate(geography = factor(x=geography, levels = county_ord)) |>
+                                                                                   arrange(year, geography),
+                                                                                 x = "geography", y = "share", fill = "geography", tog = "year", 
+                                                                                 dec = 1, title = "Bike to Work", esttype = "percent", color = "jewel")})
+    
+    output$bike_ms_race_chart <- renderEcharts4r({echart_pictorial(df= commute_data |> 
+                                                                     filter(geography_type=="Race" & year == current_census_year & variable == "Bicycle") |>
+                                                                     mutate(grouping = str_wrap(grouping, 15)),
+                                                                   x="grouping", y="share", tog="variable", 
+                                                                   icon=fa_biking, 
+                                                                   color=psrc_colors$gnbopgy_5[[4]],
+                                                                   title="Race & Hispanic Origin", dec = 2, esttype = "percent")})
+    
+    output$bike_ms_metro_chart <- renderEcharts4r({echart_bar_chart(df=commute_data |>
+                                                                      filter(geography_type == "Metro Areas" & variable == "Bike") |>
+                                                                      mutate(year = factor(x=year, levels=yr_ord)) |>
+                                                                      arrange(year, share), 
+                                                                    title = "Bike to Work", tog = 'year',
+                                                                    y='share', x='geography', esttype="percent", dec=1, color = 'jewel')})
+    
+    output$bike_ms_city_chart <- renderEcharts4r({echart_multi_series_bar_chart(df=commute_data |>
+                                                                                  filter(geography_type == "City" & variable == "Bike" & year == current_census_year) |>
+                                                                                  arrange(share) |>
+                                                                                  mutate(estimate = share) |>
+                                                                                  mutate(metric = "Bike Share to Work"), 
+                                                                                x = "geography", y='estimate', fill='metric', 
+                                                                                esttype="percent", title="Bike to Work", dec=1, color = psrc_colors$pognbgy_5[[4]])})
     
     # Tab layout
     output$biketab <- renderUI({
       tagList(
-        fluidRow(column(6,strong(tags$div(class="chart_title","Number of People Making a Bicycle Commute by County"))),
-                 column(6,strong(tags$div(class="chart_title","Share of People Making a Bicycle Commute by County")))),
-        fluidRow(column(6,plotlyOutput(ns("commute_mode_bike_est_chart"))),
-                 column(6,plotlyOutput(ns("commute_mode_bike_share_chart")))),
-        fluidRow(column(6,tags$div(class="chart_source","Source: US Census PUMS Data, 2011-2016 & 2017-2021")),
-                 column(6,tags$div(class="chart_source","Source: US Census PUMS Data, 2011-2016 & 2017-2021"))),
-        br(),
-        hr(style = "border-top: 1px solid #000000;")
+        h1("Bike to Work: County"),
+        textOutput(ns("bike_share_county_text")) |> withSpinner(color=load_clr),
+        fluidRow(column(12,echarts4rOutput(ns("bike_ms_county_chart")))),
+        tags$div(class="chart_source","Source: ACS 5yr Data Table B08301 for King, Kitsap, Pierce and Snohomish counties"),
+        hr(),
+        
+        h1("Bike to Work: Race & Ethnicity"),
+        textOutput(ns("bike_share_race_text")),
+        fluidRow(column(12,echarts4rOutput(ns("bike_ms_race_chart"), height = "600px"))),
+        tags$div(class="chart_source","Source: US Census Bureau 5-yr PUMS Variable JWTRNS for King, Kitsap, Pierce and Snohomish counties"),
+        hr(),
+        
+        h1("Bike to Work: Metropolitan Region"),
+        textOutput(ns("bike_share_metro_text")),
+        fluidRow(column(12,echarts4rOutput(ns("bike_ms_metro_chart"), height = "600px"))),
+        tags$div(class="chart_source","Source: ACS 5yr Data Table B08301"),
+        hr(),
+        
+        h1("Bike to Work: City"),
+        textOutput(ns("bike_share_city_text")),
+        fluidRow(column(12,echarts4rOutput(ns("bike_ms_city_chart"), height = "1000px"))),
+        tags$div(class="chart_source","Source: ACS 5yr Data Table B08301"),
+        hr()
       )
     })
   }) # end moduleServer
